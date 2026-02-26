0445327025b002d77474a3970237420d6449e349
Paula Gajardo <paulagajardosch@gmail.com>
2026-02-26 14:00:08 -0300

    Creando vistas

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/playas">Nuestras Playas</router-link>
+
   </nav>
   <router-view/>
 </template>
